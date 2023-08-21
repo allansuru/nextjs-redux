cd27f66094a040e9d478f1e8708245c6ab979385
“allansuru” <allansuru@hotmail.com>
2023-08-21 09:44:18 -0300

    mockando auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { post } from '../../../core/services/api.service'
 
 export default NextAuth({
   providers: [
@@ -13,18 +14,29 @@ export default NextAuth({
       name: 'NextAuthCredentials',
       credentials: {},
       async authorize(credentials) {
-        console.log(credentials)
 
-        // bater na api.post(signin, credentials).
-        // criar um state do meu profile
-        // depois remover esse mock e retornar oq tiver no post
-
-
-        return {
-          name: "Suru",
+        const myCredentials = {
+          id: 1,
+          name: "Allan SuRu",
           email: "allansuru23@gmail.com",
           image: "https://lh3.googleusercontent.com/a/AAcHTtebZfJ4cmVv8hzkwZQsJ1gFatiHDPKv9U-fk6v_W3lLhTQ=s96-c"
         }
+
+
+
+        const response = await post('signin', myCredentials)
+        console.log(response)
+
+        if (response) {
+          const data = await response
+          return data; // Retorna os dados do usuário autenticado
+        } else {
+          // Trate os erros de autenticação aqui
+          throw new Error("Erro ao autenticar");
+        }
+
+
+
       }
     })
   ],
